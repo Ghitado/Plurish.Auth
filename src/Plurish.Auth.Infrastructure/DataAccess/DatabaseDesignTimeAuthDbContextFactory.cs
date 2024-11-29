9c5ece1df17cd7478dde39a505b577c1d8d0dc9e
Ghitado <thiaguinho.020405@gmail.com>
2024-11-29 09:22:50 -0300

    feat(infrastructure): add design-time factory for AuthDbContext

---
+++ src/Plurish.Auth.Infrastructure/DataAccess/DatabaseDesignTimeAuthDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Plurish.Auth.Infrastructure.Extensions;
+
+namespace Plurish.Auth.Infrastructure.DataAccess;
+public class DatabaseDesignTimeAuthDbContextFactory : IDesignTimeDbContextFactory<AuthDbContext>
+{
+    public AuthDbContext CreateDbContext(string[] args)
+    {
+        var basePath = Path.Combine(Directory.GetCurrentDirectory(), "..", "Plurish.Auth.Api");
+
+        IConfiguration configuration = new ConfigurationBuilder()
+        .SetBasePath(basePath) 
+        .AddJsonFile("appsettings.Development.json", optional: false, reloadOnChange: true) 
+        .Build();
+
+        var builder = new DbContextOptionsBuilder<AuthDbContext>();
+        var connectionString = configuration.ConnectionString();
+        builder.UseSqlServer(connectionString);
+        return new AuthDbContext(builder.Options);
+    }
+}
