7aeee6de4448f2eed42a513293c788f21673b081
Ghitado <thiaguinho.020405@gmail.com>
2024-11-29 09:18:04 -0300

    feat(auth): add new methods to IUserRepository

--- src/Plurish.Auth.Domain/Repositories/IUserRepository.cs
+++ src/Plurish.Auth.Domain/Repositories/IUserRepository.cs
@@ -3,7 +3,8 @@
 namespace Plurish.Auth.Domain.Repositories;
 public interface IUserRepository
 {
+    Task<User> GetByEmailAsync(string email);
     Task<bool> CreateAsync(User user);
-    Task<User?> GetByUsernameOrEmailAsync(string usernameOrEmail);
-    Task<User?> GetByIdAsync(Guid userId);
+    Task<bool> UpdateAsync(User user);
+    Task DeleteAsync(Guid userId);
 }
