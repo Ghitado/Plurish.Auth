d07bca343b78507e9e7e446f42f4217601efe862
Ghitado <thiaguinho.020405@gmail.com>
2024-11-29 09:24:03 -0300

    feat(infrastructure): add ConfigurationExtension for connection string retrieval

---
+++ src/Plurish.Auth.Infrastructure/Extensions/ConfigurationExtension.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Plurish.Auth.Infrastructure.Extensions;
+public static class ConfigurationExtension
+{
+    public static string ConnectionString(this IConfiguration configuration)
+    {
+        return configuration.GetConnectionString("DefaultConnection")!;
+    }
+}
+
