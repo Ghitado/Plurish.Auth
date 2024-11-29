ff1faffd27a013cde502427f3e9ac528f9f8a06f
Ghitado <thiaguinho.020405@gmail.com>
2024-11-29 09:14:21 -0300

    feat(auth): add Desactive method to EntityBase

--- src/Plurish.Auth.Domain/Entities/EntityBase.cs
+++ src/Plurish.Auth.Domain/Entities/EntityBase.cs
@@ -1,10 +1,15 @@
-﻿using System.Data;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Plurish.Auth.Domain.Entities;
 public class EntityBase
 {
     public Guid Id { get; } = Guid.NewGuid();
     public DateTime CreatedOn { get; } = DateTime.UtcNow;
-    public bool IsActive { get; set; } = true;
+    public bool IsActive { get; private set; } = true;
+
+    public void Desactive()
+    {
+        IsActive = false;
+    }
 }
 
