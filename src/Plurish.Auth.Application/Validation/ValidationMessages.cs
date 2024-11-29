a3932ec9f7b87137e342ee353edb3270783e970e
Ghitado <thiaguinho.020405@gmail.com>
2024-11-29 09:40:02 -0300

    feat(application): add validation messages constants for user input

---
+++ src/Plurish.Auth.Application/Validation/ValidationMessages.cs
@@ -0,0 +1,9 @@
+﻿namespace Plurish.Auth.Application.Validation;
+public static class ValidationMessages
+{
+    public const string RequiredField = "This field is required.";
+    public const string MaxUsernameLength = "Username must be at most 100 characters.";
+    public const string InvalidEmail = "Invalid email format.";
+    public const string PasswordLength = "Password must be between 6 and 50 characters.";
+    public const string InvalidRole = "Invalid role.";
+}
