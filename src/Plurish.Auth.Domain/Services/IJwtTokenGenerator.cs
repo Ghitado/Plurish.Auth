e88521e36ecad1921e861ed0ac4b6d299131fa0c
Ghitado <thiaguinho.020405@gmail.com>
2024-11-29 09:18:45 -0300

    feat(auth): add IJwtTokenGenerator service

---
+++ src/Plurish.Auth.Domain/Services/IJwtTokenGenerator.cs
@@ -0,0 +1,9 @@
+﻿using Plurish.Auth.Domain.Entities;
+
+namespace Plurish.Auth.Domain.Services;
+public interface IJwtTokenGenerator
+{
+    string GenerateAccessToken(User user);
+    string GenerateRefreshToken();
+}
+
